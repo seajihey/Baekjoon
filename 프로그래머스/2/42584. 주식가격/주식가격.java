ccd3eed0214d079ad551ed95a14c0c4a2d917127
seo ji hye <48884794+seajihey@users.noreply.github.com>
2026-05-12 08:47:51 +0900

    [level 2] Title: 주식가격, Time: 12.52 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < prices.length; i++) {
+
+            // 가격 떨어졌으면 답 계산
+            while (!stack.isEmpty() &&
+                   prices[stack.peek()] > prices[i]) {
+
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+
+            stack.push(i);
+        }
+
+        // 끝까지 안 떨어진 애들 처리
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = prices.length - 1 - idx;
+        }
+
+        return answer;
+    }
+}
